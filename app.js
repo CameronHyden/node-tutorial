69210cfc3f56cb273340c781627ecc47ff759807
CameronHyden <camh.hyden@gmail.com>
2022-05-31 09:30:10 +0100

    blocking code

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const { FORMERR } = require("dns");
+const http = require("http");
 
+const server = http.createServer((reg, res) => {
+  if (reg.url === "/") {
+    res.end("home page");
+  }
+  if (req.url === "/about") {
+    //BLOCKING CODE
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; j < 1000; j++) {
+        console.log(`${j} ${i}`);
+      }
+    }
+    res.end("about page");
+  }
+  res.end("error");
+});
+
+server.listen(5000, () => {
+  console.log("server is listening on port 5000.....");
+});
